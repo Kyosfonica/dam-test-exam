de298781bc42d25f53495d8d8ccefe48ae3d07eb
Carlos Clavero <Kyosfonica@users.noreply.github.com>
2026-05-13 20:28:32 +0200

    feat: add footer component with dynamic year to main layout

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,9 @@
-    </main>
+</main>
 
-    <footer class="container">
-        <hr>
-        <p><small>Examen Desarrollo de Aplicaciones Multiplataforma (Versión Simplificada) - <?php echo date('Y'); ?></small></p>
-    </footer>
+<footer class="container">
+    <hr>
+    <p><small>Examen Desarrollo de Aplicaciones Multiplataforma - <?php echo date('Y'); ?></small></p>
+</footer>
 </body>
+
 </html>
